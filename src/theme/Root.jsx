bc16870f59f25ed4fe60d01764478265512a2779
Seth Falco <seth@falco.fun>
2026-04-23 20:25:50 +0100

    feat: add cookie consent banner

--- src/theme/Root.jsx
+++ src/theme/Root.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import CookieConsentWrapper from '../components/CookieConsentWrapper';
 import SpriteSheet from '../components/SpriteSheet';
 
 export default function Root({ children }) {
   return (
     <>
       <SpriteSheet />
+      <CookieConsentWrapper />
       {children}
     </>
   );
